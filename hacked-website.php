56afd3f67d185f99da1907c0f60cfd20952c3c03
ConstableBall <82506089+ConstableBall@users.noreply.github.com>
2022-11-12 18:39:40 -0800

    Create hacked-website.php

---
+++ hacked-website.php
@@ -0,0 +1,20 @@
+<head>
+        <title>Hacked by KW VMW</title>
+        <style>
+            iframe {
+                    display:none;
+            }
+        </style>
+</head>
+
+<body background="https://i.imgur.com/5rZ91h5.gif">
+        <iframe width="200" height="113" src="https://www.youtube.com/embed/hZsaPu-kthY?playlist=hZsaPu-kthY&amp;autoplay=1&amp;loop=1&amp;rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+        <center>
+                <h1 style=color:red;>YOU NEED CONSISTENT IT OPERATION!!!</h1>
+                <h2 style=color:green;>This website is H4CKED by KW VMW</h2>
+                <img src="https://i.imgur.com/OhoiDOT.png">
+                <h3 style=color:grey; >***** CLOUD MANAGEMENT *****</h3>
+                <p style=color:grey; >Credit to D3W3Y</p>
+        </center>
+</body>
+</html>
